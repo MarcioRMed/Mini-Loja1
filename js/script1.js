7c5562a7875c14cd6ac346275bf65b06f4ef6e5e
Márcio Ramos Medeiros <80805761+MarcioRMed@users.noreply.github.com>
2022-02-16 06:20:01 -0300

    ajust

--- js/script1.js
+++ js/script1.js
@@ -1,21 +1,60 @@
 class Produto{
   constructor(){
-    this.id = 0;
-    this.nomeProduto = '';
-    this.valor = 0;
+    this.id = 1;    
+    this.arrayProdutos = [];
   }
 
+salvar(){
+  let produto = this.lerDados();
 
-adicionar(){
+ if(this.validaCampos(produto)){
+   this.adicionar(produto);
+ }
 
-  confirm('vamos adicionar produtos!');
+  console.log(this.arrayProdutos)
+  
 
 }
 
-excluir(){
 
-alert('item deletado')
+adicionar(produto){
+  this.arrayProdutos.push(produto);
+  this.id++;
 
+
+}
+
+lerDados(){
+  let produto = {}
+  produto.id = this.id;
+  produto.nomeProduto = document.getElementById('produto').value;
+  produto.preco = document.getElementById('preco').value;
+  return produto;
+
+  
+}
+
+validaCampos(produto){
+  let msg = '';
+
+  if(produto.nomeProduto == ''){
+    msg += '- Informe o nome do Produto \n';
+  }
+
+  if(produto.preco == ''){
+    msg += '- Informe o Preço  do Produto \n';
+  }
+
+  if(msg != ''){
+    alert(msg);
+    return false
+  }
+
+  return true;
+}
+
+
+cancelar(){
 
 }
 
