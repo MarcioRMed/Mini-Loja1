4b004da1007665df2c7cb8c4c480f6eb4a05732a
Márcio Ramos Medeiros <80805761+MarcioRMed@users.noreply.github.com>
2022-02-16 05:12:46 -0300

    ajust

---
+++ js/script1.js
@@ -0,0 +1,26 @@
+class Produto{
+  constructor(){
+    this.id = 0;
+    this.nomeProduto = '';
+    this.valor = 0;
+  }
+
+
+adicionar(){
+
+  confirm('vamos adicionar produtos!');
+
+}
+
+excluir(){
+
+alert('item deletado')
+
+
+}
+
+
+
+}
+
+var produto = new Produto(); 
